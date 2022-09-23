b837f92276469fea5819d4edc0879011155e6dab
Harshal609 <patelharshal321@gmail.com>
2022-09-22 17:23:27 -0700

    Rock Paper Scissors Complete!

--- Team.py
+++ Team.py
@@ -1,5 +1,5 @@
 # ask the user for an input
-
+import random
 
 def main():
     # ask the user for an input
@@ -26,9 +26,32 @@ def main():
         exit()
 
 
-# rock, paper, scissors
+# rock, paper, scissors ++++++++++++++++++++++++++++++++STARTS HERE ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 def rockpaper():
-    print("Hello from a function")
+    # Print selection options for the user inputs
+    print("Welcome to Rock Paper Scissor!\nInput 1 for Rock , 2 for Paper , 3 for scissors!")
+    # Take the users input
+    inp = int(input("Make a selection: "))
+    # Computer makes a random choice
+    comp = random.randint(1,3)
+    # if...elif statement to decide the winner and print it
+    if (inp == comp): 
+        print("Draw !!")
+    elif (inp == 1 and comp == 2 ):
+        print("Computer Wins")
+    elif (inp == 1 and comp == 3):
+        print("Player Wins")
+    elif (inp == 2 and comp == 3 ):
+        print("Computer Wins")
+    elif (inp == 2 and comp == 1):
+        print("Player Wins")
+    elif (inp == 3 and comp == 1 ):
+        print("Computer Wins")
+    elif (inp == 3 and comp == 2):
+        print("Player Wins")
+
+# Rock, Paper, Scissors ++++++++++++++++++++++++++++++++ENDS HERE ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 # hangman
 def hangman():
