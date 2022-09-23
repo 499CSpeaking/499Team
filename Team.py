4a50f1160183af4b8fc43bfab46a30afd48cad67
Colin P <colin.p.pereira@gmail.com>
2022-09-22 17:25:28 -0700

    Completed three cup monte implementation

--- Team.py
+++ Team.py
@@ -1,3 +1,5 @@
+import random
+
 # ask the user for an input
 
 
@@ -10,7 +12,8 @@ def main():
     print("1. Rock, Paper, Scissors")
     print("2. Hangman")
     print("3. Tic Tac Toe")
-    print("4. Exit")
+    print("4. Three Cup Monte")
+    print("5. Exit")
     game = input()
     # if they want to play rock, paper, scissors
     if game == "1":
@@ -21,8 +24,11 @@ def main():
     # if they want to play tic tac toe
     elif game == "3":
         tictactoe()
-    # if they want to exit
+    # if they want to play three cup monte
     elif game == "4":
+        threecup()
+    # if they want to exit
+    elif game == "5":
         exit()
 
 
@@ -37,6 +43,31 @@ def hangman():
 # tic tac toe
 def tictactoe():
     print("Hello from a function")
+
+# three cup monte
+def threecup():
+    ball = random.randint(1,3)
+    if ball == 1:
+        print(" /\\"+" "+"  /\\"+" "+"  /\\"
+        +"\n"+"/"+str(ball)+" \\"+" "+"/  \\"+" "+"/  \\")
+    elif ball == 2:
+        print(" /\\"+" "+"  /\\"+" "+"  /\\"
+        +"\n"+"/  \\"+" "+"/"+str(ball)+" \\"+" "+"/  \\")
+    elif ball == 3:
+        print(" /\\"+" "+"  /\\"+" "+"  /\\"
+        +"\n"+"/  \\"+" "+"/  \\"+" "+"/"+str(ball)+" \\")
+    print("ball is in cup #"+str(ball))
+    ball = random.randint(1,3)
+    print("the cups are shuffled!")
+    ball = random.randint(1,3)
+    print(" /\\"+" "+"  /\\"+" "+"  /\\"
+        +"\n"+"/ \\"+" "+"/  \\"+" "+"/  \\")
+    print("Guess which cup its in: 1, 2 or 3?")
+    guess = input()
+    if int(guess) == ball:
+        print("You win!")
+    else:
+        print("You lose! Hold that L")
 
 # call the main function
 main()
